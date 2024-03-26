daafa8909f50458804db1c5cae19fd8a02e0b012
Pegasus <aniscima@gmail.com>
2024-03-26 18:55:20 +0530

    Addd origin backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,10 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    //Add your origins here
+    origin: ['http://localhost:3000'],
+  });
   await app.listen(3333);
 }
 bootstrap();
